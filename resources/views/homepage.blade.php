71e896bd890b3dd325b68542bf908075a611ded5
axemen143 <axemen143@gmail.com>
2022-11-08 23:14:17 +0800

    Changed Values of Homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,7 +9,7 @@
                 <!-- Masthead Avatar Image-->
                 <img class="masthead-avatar mb-5" src="assets-homepage/assets/img/avataaars.svg" alt="..." />
                 <!-- Masthead Heading-->
-                <h1 class="masthead-heading text-uppercase mb-0">Start Bootstrap</h1>
+                <h1 class="masthead-heading text-uppercase mb-0">YUL FRANCIS G. DEBERTO</h1>
                 <!-- Icon Divider-->
                 <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
@@ -17,7 +17,7 @@
                     <div class="divider-custom-line"></div>
                 </div>
                 <!-- Masthead Subheading-->
-                <p class="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p>
+                <p class="masthead-subheading font-weight-light mb-0">Student - Aspirant Web Designer - BSIS</p>
             </div>
         </header>
         <!-- Portfolio Section-->
